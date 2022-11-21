d30e91ad7900bd3134dc9c5ca136278d03b736c4
Rune Bohnstedt <58289859+creative-break@users.noreply.github.com>
2022-11-21 14:34:03 +0100

    Created Header
    Made a header component for the header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>
+      <div class = "header">
+        <img src="../assets/workrsFontLogo.svg" alt="workrsFont">
+      </div>
+    </h1>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped >
+.header {
+  scale: 50%;
+}
+</style>
